292419e9cad664eb04aa3413ab9621ee9f64efe5
maya <maya.salcedo07@gmail.com>
2021-10-26 15:34:34 +0300

    used useState to tally update the score

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,55 +1,50 @@
-import React, { useState } from 'react';
-
-let player1Score = 0;
-let player2Score = 0;
-
-const identifiesWinner = (card1, card2) => {
-  const valueOptions = [
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '10',
-    'JACK',
-    'QUEEN',
-    'KING',
-    'ACE',
-  ];
-
-  const card1ValueIndex = valueOptions.indexOf(card1.value);
-  const card2ValueIndex = valueOptions.indexOf(card2.value);
-
-  if (card1ValueIndex > card2ValueIndex) {
-    player1Score = player1Score + 1;
-    return 'card 1 wins';
-  } else if (card1ValueIndex < card2ValueIndex) {
-    player2Score = player2Score + 1;
-    return 'card 2 wins';
-  } else {
-    return "it's a tie";
-  }
-};
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
   const [deckId, setDeckId] = useState();
   const [remainingCard, setRemainingCard] = useState(null);
   const [player1, setPlayer1] = useState({});
   const [player2, setPlayer2] = useState({});
-
+  const [player1Score, setPlayer1Score] = useState(0);
+  const [player2Score, setPlayer2Score] = useState(0);
   const [disable, setDisable] = useState(false);
 
-  const handleClick = async () => {
+  const identifiesWinner = (card1, card2) => {
+    console.log(card1.value);
+    console.log(card2.value);
+    const valueOptions = [
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      'JACK',
+      'QUEEN',
+      'KING',
+      'ACE',
+    ];
+
+    const card1ValueIndex = valueOptions.indexOf(card1.value);
+    const card2ValueIndex = valueOptions.indexOf(card2.value);
+
+    return card1ValueIndex > card2ValueIndex
+      ? setPlayer1Score(player1Score + 1)
+      : setPlayer2Score(player2Score + 1);
+  };
+
+  async function handleClick() {
+    setDisable(false);
+    setRemainingCard(52);
     const response = await fetch(
       'https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/'
     );
     const data = await response.json();
     setDeckId(data?.deck_id);
-    setRemainingCard(data?.remainingCard);
-  };
+  }
 
   const handleDraw = async () => {
     const response = await fetch(
@@ -60,9 +55,8 @@ export default function Home() {
     setPlayer2(data?.cards[1]);
     setRemainingCard(data?.remaining);
     setDisable(data?.remaining < 40 && !disable);
+    identifiesWinner(player1, player2);
   };
-
-  const results = identifiesWinner(player1, player2);
 
   return (
     <div>
@@ -72,11 +66,10 @@ export default function Home() {
       </button>
       <img src={player1?.image} alt={`${player1?.suit}-${player1?.value}`} />
       <img src={player2?.image} alt={`${player2?.suit}-${player2?.value}`} />
-      <h2>{results}</h2>
-      <h3>Remaing Cards: {remainingCard}</h3>
+      <h2>HEllo</h2>
+      <h3>Remaining Cards: {remainingCard}</h3>
       <p>Player 1 Score: {player1Score}</p>
       <p>Player 2 Score: {player2Score}</p>
-      {console.log(player1Score)}
     </div>
   );
 }
